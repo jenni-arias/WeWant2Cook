dc9b81904acc1c1dc989994f7fbe511ae72d77e5
Marta <martabarrachina1@hotmail.com>
2017-12-04 15:46:08 +0100

    El Layout tiene una lista con recetas por defecto y un botón que no hace nada

--- app/src/main/java/arias/jenifer/wewant2cook/RecipesActivity.java
+++ app/src/main/java/arias/jenifer/wewant2cook/RecipesActivity.java
@@ -2,12 +2,46 @@ package arias.jenifer.wewant2cook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipesActivity extends AppCompatActivity {
+
+    private ArrayList<String> RecipesList;
+    private ListView list;
+    private Button btn_add;
+    private ArrayAdapter<String> recipes_adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+
+        list = (ListView)findViewById(R.id.recipes_list);
+        btn_add = (Button)findViewById(R.id.btn_add);
+
+        RecipesList = new ArrayList<>();
+        RecipesList.add("Toritlla de patatas");
+        RecipesList.add("Pastel de Queso");
+        RecipesList.add("Brownies");
+        RecipesList.add("Pasta a la Carbonara");
+        RecipesList.add("Puré de verduras");
+        RecipesList.add("Tortitas");
+
+        recipes_adapter = new ArrayAdapter<String>(     // Creamos el Adapter
+                this,
+                android.R.layout.simple_list_item_1,
+                RecipesList
+        );
+
+        list.setAdapter(recipes_adapter);
+
+
     }
 }
