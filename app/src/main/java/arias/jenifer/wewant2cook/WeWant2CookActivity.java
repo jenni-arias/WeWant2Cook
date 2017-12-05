8f45cbb7c062130e1fe1838877cbe24f56b44793
h.de.la.nieta.marin <hugolonima@gmail.com>
2017-12-05 19:57:44 +0100

    WeWant2CookLayout

--- app/src/main/java/arias/jenifer/wewant2cook/WeWant2CookActivity.java
+++ app/src/main/java/arias/jenifer/wewant2cook/WeWant2CookActivity.java
@@ -2,6 +2,8 @@ package arias.jenifer.wewant2cook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class WeWant2CookActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class WeWant2CookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_we_want_2_cook);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        onCreateOptionsMenu(toolbar.getMenu());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
