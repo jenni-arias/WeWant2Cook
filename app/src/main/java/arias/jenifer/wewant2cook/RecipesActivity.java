81e243e34ec0b19cbd652ecdbed494da0ec5b2af
Marta <martabarrachina1@hotmail.com>
2017-12-06 17:44:55 +0100

    Enlace entre RecipeActivity y IngredientsActivity  ( comentado )

--- app/src/main/java/arias/jenifer/wewant2cook/RecipesActivity.java
+++ app/src/main/java/arias/jenifer/wewant2cook/RecipesActivity.java
@@ -1,5 +1,6 @@
 package arias.jenifer.wewant2cook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -60,6 +61,23 @@ public class RecipesActivity extends AppCompatActivity {
 
             }
         });
-
     }
+
+    // Enlace de RecipesActivity a IngredientsActivity
+
+    /*private void add_recipes(View view){
+        Intent intent = new Intent(this, IngredientsActivity.class);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 0:
+                if (resultCode == AppCompatActivity.RESULT_OK){
+                    String Recipe = data.getStringExtra("recipe");
+                    RecipesList.add(new Recipes_item (Recipe,false));
+                }
+        }
+    }*/
 }
