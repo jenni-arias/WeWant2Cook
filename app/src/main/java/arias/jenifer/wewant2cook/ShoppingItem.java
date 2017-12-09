1bb2f5f00e51f12a441db8f1b0e322871ab2b71d
jenni-arias <arias.jenni@gmail.com>
2017-12-09 15:51:02 +0100

    Layout con una lista de items por defecto.

--- app/src/main/java/arias/jenifer/wewant2cook/ShoppingItem.java
+++ app/src/main/java/arias/jenifer/wewant2cook/ShoppingItem.java
@@ -1,13 +1,39 @@
 package arias.jenifer.wewant2cook;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class ShoppingItem extends AppCompatActivity {
+public class ShoppingItem {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_item);
+    private String nombre;
+    private int cantidad;
+    private String unidades;
+
+    public ShoppingItem(String nombre, int cantidad, String unidades) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.unidades = unidades;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String text) {
+        this.nombre = text;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(String unidades) {
+        this.unidades = unidades;
     }
 }
