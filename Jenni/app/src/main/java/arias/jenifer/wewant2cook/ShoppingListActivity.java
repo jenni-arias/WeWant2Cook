496036ee713b72269c22643f32f1663bc9bfdbe3
jenni-arias <arias.jenni@gmail.com>
2017-12-25 18:29:05 +0100

    Se pueden añadir ingredientes a la ShoppingList

--- Jenni/app/src/main/java/arias/jenifer/wewant2cook/ShoppingListActivity.java
+++ Jenni/app/src/main/java/arias/jenifer/wewant2cook/ShoppingListActivity.java
@@ -1,9 +1,16 @@
 package arias.jenifer.wewant2cook;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,7 +18,7 @@ public class ShoppingListActivity extends AppCompatActivity {
 
     private ListView shopping_list;
     private Button btn_add;
-    //  private EditText edit_item;
+    private EditText edit_item;
 
     private ArrayList<ShoppingItem> ShoppingList;
     private ShoppingListAdapter shoppinglist_adapter;
@@ -23,7 +30,7 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         shopping_list = (ListView) findViewById(R.id.shopping_list);
         btn_add = (Button) findViewById(R.id.btn_add);
-        // edit_item = (EditText) findViewById(R.id.edit_item);
+        edit_item = (EditText) findViewById(R.id.edit_item);
 
         ShoppingList = new ArrayList<>();
         ShoppingList.add(new ShoppingItem("Huevos", 4, "und"));
@@ -38,6 +45,24 @@ public class ShoppingListActivity extends AppCompatActivity {
                 ShoppingList
         );
 
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
         shopping_list.setAdapter(shoppinglist_adapter);
+
+    }
+
+    private void addItem() {
+        String item_text = edit_item.getText().toString();
+        if(!item_text.isEmpty()) {
+            ShoppingList.add(new ShoppingItem(item_text, 0, ""));
+            shoppinglist_adapter.notifyDataSetChanged();
+            edit_item.setText("");
+        }
+        shopping_list.smoothScrollToPosition(ShoppingList.size()-1);
     }
 }
