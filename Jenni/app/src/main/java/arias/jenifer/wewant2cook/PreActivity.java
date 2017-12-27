930093dedb14bbfa9f655e74ed07a2b583e34e74
jenni-arias <arias.jenni@gmail.com>
2017-12-27 18:58:47 +0100

    Creación Layout PreActivity

---
+++ Jenni/app/src/main/java/arias/jenifer/wewant2cook/PreActivity.java
@@ -0,0 +1,13 @@
+package arias.jenifer.wewant2cook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre);
+    }
+}
